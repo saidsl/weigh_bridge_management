b64411925ae5f36c078cf9bff1fb7b7018d6b81e
Said <saidsl@users.noreply.github.com>
2017-05-19 09:20:43 +0700

    Update naming to WBM
    Update naming from Andesit Karang Anyar to Weigh Bridge Management

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ import re, ast
 # get version from __version__ variable in andesit_karang_anyar/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('andesit_karang_anyar/__init__.py', 'rb') as f:
+with open('weigh_bridge_management/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
-	name='andesit_karang_anyar',
+	name='Weigh Bridge Management',
 	version=version,
-	description='ERPNext customization for Andesit Karang Anyar',
+	description='ERPNext Weigh Bridge Management App',
 	author='MN Technique',
 	author_email='support@castlecraft.in',
 	packages=find_packages(),
